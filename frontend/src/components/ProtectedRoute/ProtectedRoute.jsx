db32c5e3fbdef9110cd51e73f64b606149dc5a55
ronakptl996 <pronak391@gmail.com>
2024-04-24 11:51:15 +0530

    ref: loading spinner for protected route

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,6 +2,13 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { fetchLoggedInUserDetails } from "../../features/auth/authSlice";
+import {
+  Box,
+  CircularProgress,
+  LinearProgress,
+  Modal,
+  Stack,
+} from "@mui/material";
 
 const ProtectedRoute = ({ children }) => {
   const dispatch = useDispatch();
@@ -20,7 +27,21 @@ const ProtectedRoute = ({ children }) => {
     }
   }, [isLoading, isAuth]);
 
-  return isLoading ? <p>Loading...</p> : <>{children}</>;
+  return isLoading ? (
+    <Modal
+      open={true}
+      // onClose={handleClose}
+      aria-labelledby="parent-modal-title"
+      aria-describedby="parent-modal-description"
+    >
+      <section className="loading-wrapper">
+        <CircularProgress className="loading" />
+        <h3>Loading...</h3>
+      </section>
+    </Modal>
+  ) : (
+    <>{children}</>
+  );
 };
 
 export default ProtectedRoute;
