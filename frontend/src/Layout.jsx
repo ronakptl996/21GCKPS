33a97b3fb1d7a9467ba21796090a71d622dbfb8f
ronakptl996 <pronak391@gmail.com>
2024-04-26 11:33:13 +0530

    fixed: scroll issue

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  });
+  }, [pathname]);
   return (
     <>
       <Navbar />
