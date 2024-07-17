c1a6a86ba12968bba5c15ee787131d0909f1a8ae
Patel Ronak <pronak391@gmail.com>
2024-07-17 22:42:25 +0530

    style: admin all villages list

--- frontend/src/pages/AdminPages/AdminAllVillage/AdminAllVillage.jsx
+++ frontend/src/pages/AdminPages/AdminAllVillage/AdminAllVillage.jsx
@@ -1,12 +1,32 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchVillageData } from "../../../features/auth/authSlice";
-import { Grid } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Paper,
+  styled,
+  TableCell,
+  TableBody,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { tableCellClasses } from "@mui/material";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "rgba(255, 172, 58, 1)",
+    color: "#000000bf",
+    fontWeight: 600,
+    fontSize: 15,
+  },
+}));
 
 const AdminAllVillage = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const { villageWiseData } = useSelector((store) => store.auth);
 
@@ -14,43 +34,50 @@ const AdminAllVillage = () => {
     dispatch(fetchVillageData());
   }, []);
   return (
-    <div>
+    <>
       {villageWiseData && (
-        <Grid
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: 2,
-            justifyContent: "center",
-          }}
-        >
-          {villageWiseData.map((data) => (
-            <Grid
-              key={data.villageName}
-              className="carousel-card"
-              sx={{ width: "400px" }}
-            >
-              <h2>{data.villageName.toUpperCase()}</h2>
-              <p className="total-family">Total Family ({data.totalFamily})</p>
-              <div className="carousel-comeeti-details">
-                <div className="carousel-comeeti-gender">
-                  <h4>Sons ({data.totalSon})</h4>
-                  <h4>Daughters ({data.totalDaughter})</h4>
-                </div>
-                <div>
-                  <button
-                    className="carousel-comeeti-btn"
-                    onClick={() => navigate(`/village/${data.villageName}`)}
-                  >
-                    Know More
-                  </button>
-                </div>
-              </div>
-            </Grid>
-          ))}
-        </Grid>
+        <Box sx={{ mb: "35px" }}>
+          <Box sx={{ margin: "5px 0" }}>
+            <Typography variant="h6" fontWeight={700} color={"#000000bf"}>
+              All Village
+            </Typography>
+          </Box>
+          <TableContainer component={Paper}>
+            <Table aria-label="custom pagination table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Village Name</StyledTableCell>
+                  <StyledTableCell align="center">Total Family</StyledTableCell>
+                  <StyledTableCell align="center">Total Son</StyledTableCell>
+                  <StyledTableCell align="center">
+                    Total Daughter
+                  </StyledTableCell>
+                  <StyledTableCell></StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {villageWiseData?.map((item) => (
+                  <TableRow key={item._id}>
+                    <TableCell>
+                      {item.villageName.slice(0, 1).toUpperCase()}
+                      {item.villageName.slice(1)}
+                    </TableCell>
+                    <TableCell align="center" component="th" scope="row">
+                      {item.totalFamily}
+                    </TableCell>
+                    <TableCell align="center">{item.totalSon}</TableCell>
+                    <TableCell align="center">{item.totalDaughter}</TableCell>
+                    <TableCell align="center">
+                      <Link to={`/village/${item.villageName}`}>Link</Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       )}
-    </div>
+    </>
   );
 };
 
